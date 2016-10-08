dc07ad0ecd23058afda6e46021529ab56696cffd
Harry Arakkal <harryarakkal@gmail.com>
2016-10-08 02:51:26 -0400

    Added check to make sure that location fields are entered before requesting routes. Added Current Time.

--- app/src/main/java/com/isrhacks/godetroit/TripActivity.java
+++ app/src/main/java/com/isrhacks/godetroit/TripActivity.java
@@ -2,20 +2,26 @@ package com.isrhacks.godetroit;
 
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.icu.util.Calendar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class TripActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+public class TripActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, AdapterView.OnItemSelectedListener{
+
+        String currentTime;
         TextView timeText;
-        int tripHour;
-        int tripMin;
+        String tripTime;
+        String spinner_choice;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +35,18 @@ public class TripActivity extends AppCompatActivity implements TimePickerDialog.
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             // Apply the adapter to the spinner
             spinner.setAdapter(adapter);
-
+            ((Spinner) findViewById(R.id.time_spinner)).setOnItemSelectedListener(this);
             timeText = (TextView) findViewById(R.id.time_text);
+
+            Calendar c = Calendar.getInstance();
+            Date d = c.getTime();
+            SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-ddhh:mmZ");
+            currentTime = sdf.format(d);
+            currentTime = currentTime.substring(0, 10) + "T" + currentTime.substring(11);
         }
 
         public void setTime(View view){
+            //TODO Set time to current time.
             int hour = 12;
             int minute = 00;
             new TimePickerDialog(this, this, hour, minute, true).show();
@@ -41,15 +54,38 @@ public class TripActivity extends AppCompatActivity implements TimePickerDialog.
 
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // TODO Auto-generated method stub
             this.timeText.setText( hourOfDay + ":" + minute);
         }
 
         public void launchRoutes(View v){
             Intent intent = new Intent(this, RoutesActivity.class);
-            intent.putExtra("hour", tripHour);
-            intent.putExtra("min", tripMin);
-            startActivity(intent);
+            TextView fromView = (TextView) findViewById(R.id.from_input);
+            TextView toView = (TextView) findViewById(R.id.to_input);
+            String fromLocation = fromView.getText().toString();
+            String toLocation = toView.getText().toString();
+            if(fromLocation.equals("") || toLocation.equals("")){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("You have not filled all of the location fields.");
+                builder.setTitle("Error");
+                AlertDialog missingLocation = builder.create();
+                missingLocation.show();
+            }else {
+                intent.putExtra("from", fromLocation);
+                intent.putExtra("to", toLocation);
+                intent.putExtra("time_constraint", spinner_choice);
+                tripTime = currentTime;
+                intent.putExtra("time", tripTime);
+                startActivity(intent);
+            }
         }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        spinner_choice = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
